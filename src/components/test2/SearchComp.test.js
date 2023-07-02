cee9adbe8743183d3ef17108cedd27a1d0390c23
vrushal007 <vrushalkpatel007@gmail.com>
2023-07-02 16:26:02 +0530

    added search tests

---
+++ src/components/test2/SearchComp.test.js
@@ -0,0 +1,12 @@
+import { fireEvent, render, screen} from "@testing-library/react"
+import SearchComp from "./SearchComp"
+
+test('testing input search bar',()=>{
+    render(<SearchComp />)
+    const search = screen.getByText('Search:')
+    expect(search).toBeInTheDocument()
+    const display = screen.getByTestId('display')
+    expect(display).not.toHaveTextContent('Vrushal')
+    fireEvent.change(screen.getByRole('textbox'),{'target':{'value':'Vrushal'}})
+    expect(display).toHaveTextContent(/Searches for Vrushal/)
+})
